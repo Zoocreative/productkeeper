745a25c4f24ed3d225f787ec36d0ab171a570f10
Richard Walsh <richard@zoocreative.net>
2016-02-22 14:50:22 +0000

    Initial Commit

---
+++ Module.php
@@ -0,0 +1,11 @@
+<?php
+namespace mightyzoo\products;
+
+class Module extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# productkeeper
+# Product Keeper for Zoo Keeper
 Yii2 module for ZooKeeper to add Product Management
