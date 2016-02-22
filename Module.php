a0580b49f24ed3ef11549f577c7c8d9b39fc4b77
Richard Walsh <richard@zoocreative.net>
2016-02-22 17:56:44 +0000

    Update Module variables

--- Module.php
+++ Module.php
@@ -4,9 +4,19 @@ namespace mightyzoo\productkeeper;
 class Module extends \yii\base\Module
 {
 
+	/** @var array Model map */
+    public $modelMap = [];
+
+    /**
+     * @var string The prefix for user module URL.
+     *
+     * @See [[GroupUrlRule::prefix]]
+     */
+    public $urlPrefix = 'products';
+
 	/** @var array The rules to be used in URL management. */
     public $urlRules = [
-        '<id:\d+>' => 'default/update',
+        'toast/<id:\d+>' => 'default/update',
     ];
 
     public function init()
